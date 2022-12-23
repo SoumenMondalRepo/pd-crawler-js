6e276ca97d92a68fb82cc9e4c653a5e9348a55e4
Soumen Mondal <soumen.mondal@bluecore.com>
2022-12-24 02:45:31 +0530

    update

--- funcs/scrape.js
+++ funcs/scrape.js
@@ -3,12 +3,14 @@ const scrapePagerDutyAlerts = async () => {
   const { launchChrome } = require('./browser');
   const DD_URL = 'https://bluecore.datadoghq.com/dashboard'
   const PD_URL = 'https://bluecore.pagerduty.com/incidents'
+  const BC_ADMIN_URL = 'https://bluecore.com/admin/login'
 
   const dotenv = require('dotenv');
   dotenv.config();
 
   if (!process.env.GOOGLE_USERNAME || !process.env.GOOGLE_PASSWORD) throw 'Set GOOGLE_USERNAME and GOOGLE_PASSWORD';
   if (!process.env.PD_USERNAME || !process.env.PD_PASSWORD) throw 'Set GOOGLE_USERNAME and GOOGLE_PASSWORD';
+  if (!process.env.BC_ADMIN_USERNAME || !process.env.BC_ADMIN_PASSWORD) throw 'Set BC_ADMIN_USERNAME and BC_ADMIN_PASSWORD';
 
   // Flow 1 => Launching chrome and opening a new tab/page
   const [chrome, newPage, exitChrome] = await launchChrome();
@@ -17,7 +19,28 @@ const scrapePagerDutyAlerts = async () => {
   // exit the function if the tab is not properly opened
   if (!page) return;
 
-  // Flow 2 => Datadog authentication using google auth login
+  // Flow 2 => Bluecore admin authentication
+  console.log(`opening ${BC_ADMIN_URL}`);
+  try {
+    await page.goto(BC_ADMIN_URL);
+  } catch(e) {
+    console.error(`unable to visit ${BC_ADMIN_URL}`, e);
+    await exitChrome();
+    return;
+  }
+
+  //type username and password
+  await page.waitForSelector('input[name="username"]');
+  await page.type('input[name="username"]', process.env.BC_ADMIN_USERNAME); //set admin username in env file
+  await page.waitForSelector('input[type="password"]', { visible: true });
+  await page.type('input[type="password"]', process.env.BC_ADMIN_PASSWORD); //set admin pwd in env file
+
+  await Promise.all([
+    page.waitForNavigation(),
+    page.click('input[value="Log in"]'),
+  ]);
+
+  // Flow 3 => Datadog authentication using google auth login
   console.log(`opening ${DD_URL}`);
   try {
     await page.goto(DD_URL);
@@ -50,7 +73,7 @@ const scrapePagerDutyAlerts = async () => {
   ]);
 
 
-  // Flow 3 => Pagerduty authentication and incident dashboard opening
+  // Flow 4 => Pagerduty authentication and incident dashboard opening
   console.log(`opening ${PD_URL}`);
   try {
     await page.goto(PD_URL);
@@ -77,9 +100,9 @@ const scrapePagerDutyAlerts = async () => {
     page.click('a.assign-me'),
   ]);
 
-  await new Promise(resolve => setTimeout(resolve, 2000));
+  await new Promise(resolve => setTimeout(resolve, 5000));
   
-  // Flow 4 => getting a list of all open/acknowledged indicent urls
+  // Flow 5 => getting a list of all open/acknowledged indicent urls
   const incidentUrls = await page.evaluate(() => {
     const ret = []
     const items = document.querySelectorAll('.details-cell a.ember-view');
@@ -92,21 +115,31 @@ const scrapePagerDutyAlerts = async () => {
 
   console.log(`total incident urls fetched ${incidentUrls.length}`);
   
-  // Flow 5 => looping over each urls and opening then in new tab along with opening respective datadog dashboard in adjacent new tab
+  // Flow 6 => looping over each urls and opening then in new tab along with opening respective datadog dashboard in adjacent new tab
   for (let i = 0; i < incidentUrls.length; i++) {
     const page = await chrome.newPage();
     const url = incidentUrls[i];
     console.log(`opening incident ${url}`);
 
-    try {
-      await page.goto(url, {waitUntil: 'networkidle2'});
-    } catch (e) {
-      console.error(`unable to visit ${url}`, e);
-    }
+    await Promise.all([
+      await page.goto(url, {waitUntil: 'domcontentloaded'}),
+      page.waitForSelector('.incident-details-container'),
+    ])
 
     //fetch and open dashboard url
     const dashboardUrl = await page.evaluate(() => {
-      let href = $('.nagios-key:contains("dashboard_link")').siblings().find('.incident-details-container a').attr('href');
+      let href
+      let alert_type = $('.nagios-key:contains("alert_type")').siblings().find('.incident-details-container').text().trim();
+
+      if (alert_type === 'CHRONOMETER_JOB_FAILURE') {
+        href = $('.nagios-key:contains("chrono_page_link")').siblings().find('.incident-details-container a').attr('href');
+      } else if (alert_type === 'INTEGRATION_EVENT_24_HOUR' || 
+          alert_type === 'INTEGRATION_EVENT_7_DAY' || 
+          alert_type === 'ALL_INTEGRATION_EVENTS_4_HOUR' || 
+          alert_type === 'IDENTIFY_EVENT_7_DAY' ||
+          alert_type === 'CHRONOMETER_EVENT_7_DAY') {
+        href = $('.nagios-key:contains("dashboard_link")').siblings().find('.incident-details-container a').attr('href');
+      }
 
       return href;
     });
@@ -116,7 +149,7 @@ const scrapePagerDutyAlerts = async () => {
       console.log(`opening dashboard ${dashboardUrl}`);
 
       try {
-        await page.goto(dashboardUrl, {waitUntil: 'networkidle2'});
+        await page.goto(dashboardUrl, {waitUntil: 'domcontentloaded'});
       } catch (e) {
         console.error(`unable to visit ${dashboardUrl}`, e);
       }
@@ -125,6 +158,21 @@ const scrapePagerDutyAlerts = async () => {
 
   //await exitChrome(); // close chrome
   return;
+};
+
+const identifyAlertType = () => {
+  return {
+    CHRONOMETER_JOB_FAILURE: 'chrono failure',
+    INTEGRATION_EVENT_24_HOUR: '',
+    INTEGRATION_EVENT_7_DAY: '',
+    ALL_INTEGRATION_EVENTS_4_HOUR: '',
+    IDENTIFY_EVENT_7_DAY: '',
+    JUSTUNO_EVENT_24_HOUR: '',
+    UNSUBSCRIBE_BY_SOURCE_7_DAYS: '',
+    RECURRING_EMAIL_VOLUME_7_DAY: '',
+    CHRONOMETER_EVENT_7_DAY: '',
+    TRANSACTIONAL_EMAIL_VOLUME_4_HOUR: '',
+  }
 };
 
 module.exports = scrapePagerDutyAlerts;
